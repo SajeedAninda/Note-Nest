f688076ce7c002018d1111c3a4176aa2d64538f6
SajeedAninda <sajaninda@gmail.com>
2025-03-12 17:27:29 +0600

    Added Note Cards

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import FolderCards from '../FolderCards/FolderCards'
+import NoteCards from '../NoteCards/NoteCards'
 
 const Discover = () => {
   return (
-    <div className='bg-[#f7f8fa] w-[full] h-full mt-6 rounded-xl px-6 py-10'>
-      <h1 className='text-[#242627] font-bold text-[25px]'>Recent Folders</h1>
-      <div className='flex mt-3 gap-3 items-center'>
-        <p className='text-[#242627] font-semibold text-[18px]'>Today</p>
-        <p className='text-[#242627] font-semibold text-[18px] underline'>This Week</p>
-        <p className='text-[#242627] font-semibold text-[18px]'>This Month</p>
+    <div className=' bg-[#f7f8fa] mt-6'>
+      <div className='folders w-[full] h-full rounded-xl px-6 pt-6'>
+        <h1 className='text-[#242627] font-bold text-[25px]'>Recent Folders</h1>
+        <div className='flex mt-3 gap-3 items-center'>
+          <p className='text-[#242627] font-semibold text-[18px]'>Today</p>
+          <p className='text-[#242627] font-semibold text-[18px] underline'>
+            This Week
+          </p>
+          <p className='text-[#242627] font-semibold text-[18px]'>This Month</p>
+        </div>
+        <div className='folderCards'>
+          <FolderCards></FolderCards>
+        </div>
       </div>
-      <div className='folderCards'>
-        <FolderCards></FolderCards>
+
+      <div className='notes w-[full] h-full rounded-xl px-6 mt-3'>
+        <h1 className='text-[#242627] font-bold text-[25px]'>Recent Notes</h1>
+        <div className='flex mt-3 gap-3 items-center'>
+          <p className='text-[#242627] font-semibold text-[18px] underline'>
+            Today
+          </p>
+          <p className='text-[#242627] font-semibold text-[18px]'>This Week</p>
+          <p className='text-[#242627] font-semibold text-[18px]'>This Month</p>
+        </div>
+        <div className='folderCards'>
+          <NoteCards></NoteCards>
+        </div>
       </div>
     </div>
   )
