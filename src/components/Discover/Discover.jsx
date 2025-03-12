8dfdc5327ad909deabf1732e7a4d1f83aad95b1e
SajeedAninda <sajaninda@gmail.com>
2025-03-12 17:06:45 +0600

    Created discovery top part

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import FolderCards from '../FolderCards/FolderCards'
 
 const Discover = () => {
   return (
-    <div className='bg-[#f7f8fa] w-[full] h-full mt-6 rounded-tl-lg'>Lorem ipsum dol</div>
+    <div className='bg-[#f7f8fa] w-[full] h-full mt-6 rounded-xl px-6 py-10'>
+      <h1 className='text-[#242627] font-bold text-[25px]'>Recent Folders</h1>
+      <div className='flex mt-3 gap-3 items-center'>
+        <p className='text-[#242627] font-semibold text-[18px]'>Today</p>
+        <p className='text-[#242627] font-semibold text-[18px]'>This Week</p>
+        <p className='text-[#242627] font-semibold text-[18px]'>This Month</p>
+      </div>
+      <div className='folderCards mt-6'>
+        <FolderCards></FolderCards>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/FolderCards/FolderCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FolderCards = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FolderCards;
