f50bd19028fb655a269d3dd3ce8ad01cebf4efae
SajeedAninda <sajaninda@gmail.com>
2025-03-13 16:46:15 +0600

    Installed and configured firebase along with auth provider

---
+++ src/components/Authentication/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbXSHuJtf3c6qJxBry8gGh6NMZuyV95Dw",
+  authDomain: "note-nest-86fb2.firebaseapp.com",
+  projectId: "note-nest-86fb2",
+  storageBucket: "note-nest-86fb2.firebasestorage.app",
+  messagingSenderId: "300830043997",
+  appId: "1:300830043997:web:3c10d78d3baccce7e402b4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
