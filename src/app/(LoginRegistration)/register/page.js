54e0aaef7788aa593d58e95fe301cc275c949b75
SajeedAninda <sajaninda@gmail.com>
2025-03-13 17:17:32 +0600

    Completed Registration Functionality

--- src/app/(LoginRegistration)/register/page.js
+++ src/app/(LoginRegistration)/register/page.js
@@ -1,8 +1,17 @@
 'use client'
+import useAuth from '@/components/Hooks/useAuth';
+import useAxiosInstance from '@/components/Hooks/useAxiosInstance';
+import { useRouter } from "next/navigation";
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const page = () => {
-    let handleRegisterSubmit = e => {
+    let axiosInstance = useAxiosInstance()
+    let { signUp } = useAuth()
+    const router = useRouter()
+
+
+    let handleRegisterSubmit = async e => {
         e.preventDefault();
         let firstName = e.target.name.value;
         let lastName = e.target.lname.value;
@@ -11,6 +20,38 @@ const page = () => {
         let password = e.target.password.value;
         let imgUrl = e.target.imgUrl.value;
         console.log({ fullName, email, password, imgUrl })
+
+        if (password.length < 6) {
+            return toast.error('Password must be at least 6 characters!')
+        }
+        if (!/[A-Z]/.test(password)) {
+            return toast.error('Password must contain at least one capital letter!')
+        }
+        if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+            return toast.error(
+                'Password must contain at least one special character!'
+            )
+        }
+
+        let loadingToast = toast.loading('Registering...')
+
+        try {
+            let userCredential = await signUp(email, password)
+            let user = userCredential.user
+
+            let userDetails = { fullName, email, imgUrl }
+            let res = await axiosInstance.post('/userRegister', userDetails)
+
+            if (res.data.insertedId) {
+                toast.dismiss(loadingToast)
+                toast.success('Registration Successful. Please Login')
+                router.push('/login')
+            }
+        } catch (error) {
+            console.error(error)
+            toast.dismiss(loadingToast)
+            toast.error(error.message)
+        }
     }
 
     return (
@@ -23,25 +64,25 @@ const page = () => {
                 <div className="grid sm:grid-cols-2 gap-8">
                     <div>
                         <label className="text-[#242627] text-sm font-medium mb-2 block">First Name</label>
-                        <input name="name" type="text" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter name" />
+                        <input name="name" type="text" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter name" required/>
                     </div>
                     <div>
                         <label className="text-[#242627] text-sm font-medium mb-2 block">Last Name</label>
-                        <input name="lname" type="text" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter last name" />
+                        <input name="lname" type="text" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter last name" required/>
                     </div>
                     <div>
                         <label className="text-[#242627] text-sm font-medium mb-2 block">Email Id</label>
-                        <input name="email" type="text" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter email" />
+                        <input name="email" type="text" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter email" required/>
                     </div>
                     <div>
                         <label className="text-[#242627] text-sm font-medium mb-2 block">Password</label>
-                        <input name="password" type="password" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter password" />
+                        <input name="password" type="password" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter password" required/>
                     </div>
 
                 </div>
                 <div className='mt-8'>
                     <label className="text-[#242627] text-sm font-medium mb-2 block">Image Url</label>
-                    <input name="imgUrl" type="text" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter Image Url" />
+                    <input name="imgUrl" type="text" className="bg-slate-100 w-full text-[#242627] text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter Image Url" required/>
                 </div>
 
                 <div className="mt-12">
