502088adf4a331ab1ef7cf626e68d7537da89fbf
SajeedAninda <sajaninda@gmail.com>
2025-03-17 17:56:54 +0600

    Made new note page functional

--- src/components/NewNote/NewNote.jsx
+++ src/components/NewNote/NewNote.jsx
@@ -7,11 +7,18 @@ import {
   SelectTrigger,
   SelectValue
 } from '../ui/select'
+import useAuth from '../Hooks/useAuth'
+import useAxiosInstance from '../Hooks/useAxiosInstance'
+import { useQuery } from '@tanstack/react-query'
 
 const NewNote = () => {
   const [noteName, setNoteName] = useState('')
   const [noteDescription, setNoteDescription] = useState('')
+  const [selectedFolder, setSelectedFolder] = useState('no-folder')
   const [selectedColor, setSelectedColor] = useState('')
+  const { loggedInUser } = useAuth()
+  const currentUserEmail = loggedInUser?.email
+  const axiosInstance = useAxiosInstance()
 
   const colors = [
     { name: 'Red', hex: '#FF4136' },
@@ -24,30 +31,68 @@ const NewNote = () => {
     { name: 'Teal', hex: '#39CCCC' }
   ]
 
+  const {
+    data: folderData,
+    isLoading: isFolderLoading,
+    refetch
+  } = useQuery({
+    queryKey: ['folderData', currentUserEmail],
+    queryFn: async () => {
+      if (!currentUserEmail) return []
+      const response = await axiosInstance.get(
+        `/getFolders?email=${currentUserEmail}`
+      )
+      return response.data
+    },
+    enabled: !!currentUserEmail
+  })
+
   return (
-    <div className=''>
-      <div className='flex justify-between gap-6'>
+    <div>
+      <div className='w-full'>
+        <label
+          htmlFor='note-name'
+          className='block text-[18px] font-semibold mb-2 text-[#242627]'
+        >
+          Folder Name
+        </label>
+        <input
+          id='note-name'
+          value={noteName}
+          onChange={e => setNoteName(e.target.value)}
+          placeholder='Enter Note name'
+          className='mt-2 block w-full px-3 py-2 border rounded-md border-[#242627] focus:outline-none focus:ring-2 focus:ring-blue-500 min-h-[42px]'
+        />
+      </div>
+
+      <div className='flex justify-between gap-6 mt-6'>
         <div className='w-[50%]'>
-          <label
-            htmlFor='note-name'
-            className='block text-[18px] font-semibold mb-2 text-[#242627]'
-          >
-            Folder Name
+          <label className='block text-[18px] font-semibold mb-2 text-[#242627]'>
+            Note Folder
           </label>
-          <input
-            id='note-name'
-            value={noteName}
-            onChange={e => setNoteName(e.target.value)}
-            placeholder='Enter Note name'
-            className='mt-2 block w-full px-3 py-2 border rounded-md border-[#242627] focus:outline-none focus:ring-2 focus:ring-blue-500 min-h-[42px]'
-          />
+          <Select value={selectedFolder} onValueChange={setSelectedFolder}>
+            <SelectTrigger className='mt-1 w-full px-3 py-2 border rounded-md border-[#242627] focus:ring-2 focus:ring-blue-500 min-h-[42px]'>
+              <SelectValue placeholder='Select a folder' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem key='no-folder' value='no-folder'>
+                No Folder
+              </SelectItem>
+
+              {folderData?.map(folder => (
+                <SelectItem key={folder._id} value={folder.folderName}>
+                  {folder.folderName}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
 
         <div className='w-[50%]'>
           <label className='block text-[18px] font-semibold mb-2 text-[#242627]'>
             Note Theme
           </label>
-          <Select onValueChange={val => setSelectedColor(val)}>
+          <Select value={selectedColor} onValueChange={setSelectedColor}>
             <SelectTrigger className='mt-1 w-full px-3 py-2 border rounded-md border-[#242627] focus:ring-2 focus:ring-blue-500 min-h-[42px]'>
               <SelectValue placeholder='Select a color' />
             </SelectTrigger>
@@ -76,7 +121,7 @@ const NewNote = () => {
           Write Note
         </label>
         <textarea
-          id='note-name'
+          id='note-description'
           rows={5}
           value={noteDescription}
           onChange={e => setNoteDescription(e.target.value)}
