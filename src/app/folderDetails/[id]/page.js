c50951a77c8acf325b55e6a699174c4d9c0b7130
SajeedAninda <sajaninda@gmail.com>
2025-03-20 17:02:02 +0600

    Fetched a folder details

--- src/app/folderDetails/[id]/page.js
+++ src/app/folderDetails/[id]/page.js
@@ -1,13 +1,34 @@
-import React from 'react';
+"use client";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosInstance from "@/components/Hooks/useAxiosInstance";
 
-const page = ({ params }) => {
-    const { id } = params
+const Page = ({ params }) => {
+  const { id } = params;
+  const axiosInstance = useAxiosInstance();
 
-    return (
-        <div>
-            {id}
-        </div>
-    );
+  const {
+    data: folderDetailedData,
+    isLoading: isFolderLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["folderDetailedData", id],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/getFolder/${id}`);
+      return response.data;
+    },
+    enabled: !!id, 
+  });
+
+  if (isFolderLoading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>Folder ID: {id}</h1>
+      <h2>Folder Name: {folderDetailedData?.folderName}</h2>
+      <p>Created On: {folderDetailedData?.folderCreation}</p>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
