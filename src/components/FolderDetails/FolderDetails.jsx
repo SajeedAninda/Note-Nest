84644cbb645713b34b6825ab31e4fbc0afcc6076
SajeedAninda <sajaninda@gmail.com>
2025-03-20 17:11:24 +0600

    Fetched folder data in folder details page

--- src/components/FolderDetails/FolderDetails.jsx
+++ src/components/FolderDetails/FolderDetails.jsx
@@ -2,14 +2,17 @@
 import React from 'react'
 import useAxiosInstance from '../Hooks/useAxiosInstance'
 import { useQuery } from '@tanstack/react-query'
+import useAuth from '../Hooks/useAuth'
 
 const FolderDetails = ({ id }) => {
   const axiosInstance = useAxiosInstance()
+  const { loggedInUser } = useAuth()
+  const currentUserEmail = loggedInUser?.email
 
   const {
     data: folderDetailedData,
-    isLoading: isFolderLoading,
-    refetch
+    isLoading: isFolderDetailsLoading,
+    refetch: folderDetailsRefetch
   } = useQuery({
     queryKey: ['folderDetailedData', id],
     queryFn: async () => {
@@ -18,6 +21,23 @@ const FolderDetails = ({ id }) => {
     },
     enabled: !!id
   })
+
+  const {
+    data: folderData,
+    isLoading: isFolderLoading,
+    refetch: foldersRefetch
+  } = useQuery({
+    queryKey: ['folderData', currentUserEmail],
+    queryFn: async () => {
+      if (!currentUserEmail) return []
+      const response = await axiosInstance.get(
+        `/getFolders?email=${currentUserEmail}`
+      )
+      return response.data
+    },
+    enabled: !!currentUserEmail
+  })
+
   return <div></div>
 }
 
