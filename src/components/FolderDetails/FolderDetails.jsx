648da4e3e1d7e51fcb23d2cd670e4c090f76c2fc
SajeedAninda <sajaninda@gmail.com>
2025-03-21 17:06:52 +0600

    Set Notes deleting and updating functions in all folder details  page

--- src/components/FolderDetails/FolderDetails.jsx
+++ src/components/FolderDetails/FolderDetails.jsx
@@ -1,14 +1,20 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import useAxiosInstance from '../Hooks/useAxiosInstance'
 import { useQuery } from '@tanstack/react-query'
 import useAuth from '../Hooks/useAuth'
 import { RiFileEditFill } from 'react-icons/ri'
+import { MdDelete } from 'react-icons/md'
+import UpdateModal from '../UpdateModal/UpdateModal'
+import Swal from 'sweetalert2'
+import toast from 'react-hot-toast'
 
 const FolderDetails = ({ id }) => {
   const axiosInstance = useAxiosInstance()
   const { loggedInUser } = useAuth()
   const currentUserEmail = loggedInUser?.email
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [selectedNote, setSelectedNote] = useState(null)
 
   const {
     data: folderDetailedData,
@@ -26,7 +32,7 @@ const FolderDetails = ({ id }) => {
   const {
     data: notesData,
     isLoading: isNotesLoading,
-    refetch: notesRefetch
+    refetch
   } = useQuery({
     queryKey: ['notesData', currentUserEmail],
     queryFn: async () => {
@@ -61,46 +67,107 @@ const FolderDetails = ({ id }) => {
     (a, b) => new Date(b.noteCreation) - new Date(a.noteCreation)
   )
 
+  const handleNoteDelete = async note => {
+    Swal.fire({
+      title: 'Do you want to delete this Note?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#242627',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(async result => {
+      if (result.isConfirmed) {
+        const deleteResponse = await axiosInstance.delete(
+          `/noteDelete/${note._id}`
+        )
+
+        if (deleteResponse.data.deletedCount > 0) {
+          toast.success('Note deleted successfully!')
+
+          const trashResponse = await axiosInstance.post('/addToTrash', note)
+
+          if (trashResponse.data.insertedId) {
+            toast.success('Note moved to Trash!')
+            refetch()
+          } else {
+            toast.error('Failed to move note to Trash.')
+          }
+        } else {
+          toast.error('Error! Failed to delete Note.')
+        }
+      }
+    })
+  }
+
   return (
     <div>
-      <h1 className='text-[#242627] font-bold mt-2 text-[25px] text-center'>
-        {folderDetailedData?.folderName}
-      </h1>
-      <div className='flex mt-6 pb-6 gap-6 items-center'>
-        <div className='w-full'>
-          <div className='grid grid-cols-3 gap-6'>
-            {sortedNotes?.map(note => (
-              <div
-                key={note._id}
-                className='card rounded-lg px-6 py-4'
-                style={{
-                  backgroundColor: note.selectedColor,
-                  cursor: 'pointer',
-                  hover: 'opacity-70',
-                  transition: 'all 0.15s'
-                }}
-              >
-                <p className='text-[#242627] font-bold text-[12px]'>
-                  {new Date(note.noteCreation).toLocaleDateString()}{' '}
-                  <span className='px-2'>|</span>{' '}
-                  {new Date(note.noteCreation).toLocaleTimeString()}
-                  <span className='px-2'>|</span>
-                  {note.notefolder}
-                </p>
-                <div className='flex justify-between items-center mt-5 pb-3 border-b-2 border-[#242627]'>
-                  <h2 className='text-[#242627] font-bold text-[20px]'>
-                    {note.noteName}
-                  </h2>
-                  <RiFileEditFill className='text-[#242627] font-bold text-[25px]' />
+      <div>
+        <h1 className='text-[#242627] font-bold mt-2 text-[25px] text-center'>
+          {folderDetailedData?.folderName}
+        </h1>
+        <div className='flex mt-6 pb-6 gap-6 items-center'>
+          <div className='w-full'>
+            <div className='grid grid-cols-3 gap-6'>
+              {sortedNotes?.map(note => (
+                <div
+                  key={note._id}
+                  className='card rounded-lg px-6 py-4'
+                  style={{
+                    backgroundColor: note.selectedColor,
+                    cursor: 'pointer',
+                    hover: 'opacity-70',
+                    transition: 'all 0.15s'
+                  }}
+                >
+                  <p className='text-[#242627] font-bold text-[12px]'>
+                    {new Date(note.noteCreation).toLocaleDateString()}{' '}
+                    <span className='px-2'>|</span>{' '}
+                    {new Date(note.noteCreation).toLocaleTimeString()}
+                    <span className='px-2'>|</span>
+                    {note.notefolder}
+                  </p>
+                  <div className='flex justify-between items-center mt-5 pb-3 border-b-2 border-[#242627]'>
+                    <h2 className='text-[#242627] font-bold text-[20px]'>
+                      {note.noteName}
+                    </h2>
+                    <div className='flex gap-4'>
+                      <div
+                        onClick={() => {
+                          handleNoteDelete(note)
+                        }}
+                      >
+                        <MdDelete className='text-[#242627] font-bold text-[25px] hover:opacity-70' />
+                      </div>
+                      <div
+                        onClick={() => {
+                          setSelectedNote(note)
+                          setIsModalOpen(true)
+                        }}
+                      >
+                        <RiFileEditFill className='text-[#242627] font-bold text-[25px] hover:opacity-70' />
+                      </div>
+                    </div>
+                  </div>
+                  <p className='text-[#242627] font-normal mt-3 w-full text-[18px]'>
+                    {note.noteDescription}
+                  </p>
                 </div>
-                <p className='text-[#242627] font-normal mt-3 w-full text-[18px]'>
-                  {note.noteDescription}
-                </p>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
+      {isModalOpen && selectedNote && (
+        <UpdateModal
+          note={selectedNote}
+          refetch={refetch}
+          onClose={() => {
+            setIsModalOpen(false)
+            setSelectedNote(null)
+          }}
+        />
+      )}
     </div>
   )
 }
