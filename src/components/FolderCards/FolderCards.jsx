e9846eebbf02752453be0d1c3e0a3679e3f65a29
SajeedAninda <sajaninda@gmail.com>
2025-03-24 16:15:08 +0600

    Handled folder loading state and no folder state

--- src/components/FolderCards/FolderCards.jsx
+++ src/components/FolderCards/FolderCards.jsx
@@ -9,12 +9,16 @@ import Link from 'next/link'
 
 const FolderCards = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
-  const [selectedFilter, setSelectedFilter] = useState('Recent') 
+  const [selectedFilter, setSelectedFilter] = useState('Recent')
   const { loggedInUser } = useAuth()
   const currentUserEmail = loggedInUser?.email
   const axiosInstance = useAxiosInstance()
 
-  const { data: folderData, refetch } = useQuery({
+  const {
+    data: folderData,
+    refetch,
+    isLoading: folderLoading
+  } = useQuery({
     queryKey: ['folderData', currentUserEmail],
     queryFn: async () => {
       if (!currentUserEmail) return []
@@ -52,8 +56,12 @@ const FolderCards = () => {
   const isThisWeek = date => {
     const today = new Date()
     const folderDate = new Date(date)
-    const firstDayOfWeek = new Date(today.setDate(today.getDate() - today.getDay()))
-    const lastDayOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 6))
+    const firstDayOfWeek = new Date(
+      today.setDate(today.getDate() - today.getDay())
+    )
+    const lastDayOfWeek = new Date(
+      today.setDate(today.getDate() - today.getDay() + 6)
+    )
     return folderDate >= firstDayOfWeek && folderDate <= lastDayOfWeek
   }
 
@@ -75,11 +83,17 @@ const FolderCards = () => {
   if (selectedFilter === 'Recent') {
     filteredFolders = sortedFolders.slice(0, 3)
   } else if (selectedFilter === 'Today') {
-    filteredFolders = sortedFolders.filter(folder => isToday(folder.folderCreation))
+    filteredFolders = sortedFolders.filter(folder =>
+      isToday(folder.folderCreation)
+    )
   } else if (selectedFilter === 'This Week') {
-    filteredFolders = sortedFolders.filter(folder => isThisWeek(folder.folderCreation))
+    filteredFolders = sortedFolders.filter(folder =>
+      isThisWeek(folder.folderCreation)
+    )
   } else if (selectedFilter === 'This Month') {
-    filteredFolders = sortedFolders.filter(folder => isThisMonth(folder.folderCreation))
+    filteredFolders = sortedFolders.filter(folder =>
+      isThisMonth(folder.folderCreation)
+    )
   }
 
   const formatDate = date => {
@@ -105,73 +119,64 @@ const FolderCards = () => {
           </button>
         ))}
       </div>
-
+  
       <div className='flex gap-6 items-center mt-4'>
         <div className='w-[75%]'>
-          <div className='grid grid-cols-3 gap-6'>
-            {filteredFolders.length > 0 ? (
-              filteredFolders.map(folder => {
-                const folderBgColor = folder?.selectedColor || '#76dd5d'
-                const iconColor = darkenColor(folderBgColor, 30)
-
-                return (
-                  <Link
-                    href={`/folderDetails/${folder?._id}`}
-                    key={folder?._id}
-                  >
-                    <div
-                      className='card h-[200px] rounded-lg px-6 py-4 cursor-pointer hover:opacity-70 transition-all duration-150'
-                      style={{ backgroundColor: folderBgColor }}
-                    >
-                      <div className='flex justify-between items-center'>
-                        <FaNoteSticky
-                          className='text-[40px]'
-                          style={{ color: iconColor }}
-                        />
+          {folderLoading ? (
+            <div className='flex justify-center items-center'>
+              <p className='text-[#242627] font-bold text-[20px]'>Loading folders...</p>
+            </div>
+          ) : (
+            <div className='grid grid-cols-3 gap-6'>
+              {filteredFolders.length > 0 ? (
+                filteredFolders.map(folder => {
+                  const folderBgColor = folder?.selectedColor || '#76dd5d'
+                  const iconColor = darkenColor(folderBgColor, 30)
+  
+                  return (
+                    <Link href={`/folderDetails/${folder?._id}`} key={folder?._id}>
+                      <div
+                        className='card h-[200px] rounded-lg px-6 py-4 cursor-pointer hover:opacity-70 transition-all duration-150'
+                        style={{ backgroundColor: folderBgColor }}
+                      >
+                        <div className='flex justify-between items-center'>
+                          <FaNoteSticky className='text-[40px]' style={{ color: iconColor }} />
+                        </div>
+                        <h2 className='text-[#242627] font-bold text-[20px] mt-3'>{folder?.folderName}</h2>
+                        <h2 className='text-[#242627] font-semibold text-[16px] mt-3'>
+                          {formatDate(folder?.folderCreation)}
+                        </h2>
                       </div>
-                      <h2 className='text-[#242627] font-bold text-[20px] mt-3'>
-                        {folder?.folderName}
-                      </h2>
-                      <h2 className='text-[#242627] font-semibold text-[16px] mt-3'>
-                        {formatDate(folder?.folderCreation)}
-                      </h2>
-                    </div>
-                  </Link>
-                )
-              })
-            ) : (
-              <div className='flex justify-center items-center'>
-                <p className='text-[#242627] font-bold text-center flex text-[20px]'>
-                  Folder Not Available
-                </p>
-              </div>
-            )}
-          </div>
+                    </Link>
+                  )
+                })
+              ) : (
+                <div className='flex justify-center items-center'>
+                  <p className='text-[#242627] font-bold text-[20px]'>No folders available</p>
+                </div>
+              )}
+            </div>
+          )}
         </div>
-
+  
         <div className='w-[25%] p-10'>
           <div
             onClick={() => setIsModalOpen(true)}
             className='border-2 border-dotted flex-col text-center place-items-center justify-center items-center p-6 rounded-lg hover:bg-gray-200 cursor-pointer transition-all duration-150'
           >
             <FaFolderPlus className='text-center text-[#242627] font-bold text-[20px]' />
-            <h2 className='text-[#242627] font-bold text-[20px] mt-3'>
-              New Folder
-            </h2>
+            <h2 className='text-[#242627] font-bold text-[20px] mt-3'>New Folder</h2>
           </div>
         </div>
       </div>
-
+  
       {/* Modal */}
       {isModalOpen && (
-        <NewFolderModal
-          folderData={folderData}
-          refetch={refetch}
-          onClose={() => setIsModalOpen(false)}
-        />
+        <NewFolderModal folderData={folderData} refetch={refetch} onClose={() => setIsModalOpen(false)} />
       )}
     </div>
   )
+  
 }
 
 export default FolderCards
